cd8200b88f14ab8a5b6be357c048e624e2c28e47
JimmyU <hongshaoww@126.com>
2014-07-24 11:12:45 +0800

    Config

--- oUF_Jim/config.lua
+++ oUF_Jim/config.lua
@@ -19,3 +19,59 @@ G.media = {
 	bar = "Interface\\AddOns\\oUF_Jim\\media\\texture",
 }
 
+--[[------------------>>  Config  <<------------------]]--
+
+C.Font = {
+	Normal_FontSize = 12,
+	Small_FontSize = 16,
+}
+
+C.Player = { -- 玩家
+	height = 32,
+	width = 280,
+	positon = {a1 = "TOPRIGHT", parent= "UIParent", a2 = "CENTER", x = "-120", y = "-60"},
+}
+
+C.Target = { -- 目标
+	height = 32,
+	width = 280,
+	positon = {a1 = "TOPLEFT", parent= "UIParent", a2 = "CENTER", x = "120", y = "-60"},
+}
+
+C.Focus = { -- 焦点
+	height = 32,
+	width = 280,
+	positon = {a1 = "TOPLEFT", parent= "UIParent", a2 = "CENTER", x = "120", y = "60"},
+}
+
+C.Pet = { -- 宠物
+	height = 32,
+	width = 80,
+	positon = {a1 = "TOPRIGHT", parent= "UIParent", a2 = "CENTER", x = "-405", y = "-60"},
+}
+
+C.ToT = { -- 目标的目标
+	height = 32,
+	width = 80,
+	positon = {a1 = "TOPLEFT", parent= "UIParent", a2 = "CENTER", x = "405", y = "-60"},
+}
+
+C.ToF = { -- 焦点的目标
+	height = 32,
+	width = 80,
+	positon = {a1 = "TOPLEFT", parent= "UIParent", a2 = "CENTER", x = "405", y = "60"},
+}
+
+C.Boss = { -- 首领
+	height = 32,
+	width = 160,
+	positon = {a1 = "TOPRIGHT", parent= "UIParent", a2 = "RIGHT", x = "-100", y = "120"},
+	space = 80,
+}
+
+C.Arena = { -- 竞技场敌人
+	height = 32,
+	width = 160,
+	positon = {a1 = "TOPRIGHT", parent= "UIParent", a2 = "RIGHT", x = "-100", y = "120"},
+	space = 80,
+}
