e7ddba25fd336fcb8a89eade64d30be69d6390ea
JimmyU <hongshaoww@126.com>
2014-07-24 11:12:21 +0800

    Globals

--- oUF_Jim/config.lua
+++ oUF_Jim/config.lua
@@ -7,3 +7,15 @@ ns[4] = {} -- Localization
 
 local C, F, G, L = unpack(ns)
 
+--[[------------------>>  Globals  <<------------------]]--
+
+
+G.addon = "oUF_Jim_"
+
+G.font = "Interface\\AddOns\\oUF_Jim\\media\\font.ttf"
+
+G.media = {
+	blank = "Interface\\Buttons\\WHITE8x8",
+	bar = "Interface\\AddOns\\oUF_Jim\\media\\texture",
+}
+
